33a2415cdf7fd02c05455c4a458579333029dfe5
Elvan Fiernes Ernando <elvanfiernesernando@gmail.com>
2022-10-27 07:33:42 +0700

    fix naming class into className

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -11,7 +11,7 @@ export default function History() {
 
                 <div className='h-[200px] w-full bg-[#1D03BD] rounded-md absolute top-0'></div>
 
-                <div class="overflow-x-auto shadow-md relative z-[2] m-8" >
+                <div className="overflow-x-auto shadow-md relative z-[2] m-8" >
 
                     {/* <div className='flex justify-between mb-4'>
                         <h3 className='font-semibold text-white text-lg'>Categories</h3>
@@ -21,79 +21,79 @@ export default function History() {
                         </button>
                     </div> */}
 
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="text-[#1D03BD] uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead className="text-[#1D03BD] uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
-                                <th scope="col" class="py-3 px-6">
+                                <th scope="col" className="py-3 px-6">
                                     No.
                                 </th>
-                                <th scope="col" class="py-3 px-6">
+                                <th scope="col" className="py-3 px-6">
                                     Name
                                 </th>
-                                <th scope="col" class="py-3 px-6">
+                                <th scope="col" className="py-3 px-6">
                                     Created At
                                 </th>
-                                <th scope="col" class="py-3 px-6">
+                                <th scope="col" className="py-3 px-6">
                                     Updated At
                                 </th>
-                                <th scope="col" class="py-3 px-6 text-right">
+                                <th scope="col" className="py-3 px-6 text-right">
                                     Action
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="py-4 px-6 font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th scope="row" className="py-4 px-6 font-medium text-gray-500 whitespace-nowrap dark:text-white">
                                     1
                                 </th>
-                                <td class="py-4 px-6">
+                                <td className="py-4 px-6">
                                     Makanan
                                 </td>
-                                <td class="py-4 px-6">
+                                <td className="py-4 px-6">
                                     2 months ago
                                 </td>
-                                <td class="py-4 px-6">
+                                <td className="py-4 px-6">
                                     2 months ago
                                 </td>
-                                <td class="py-4 px-6 flex justify-end gap-4">
-                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                    <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
+                                <td className="py-4 px-6 flex justify-end gap-4">
+                                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    <a href="#" className="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
                                 </td>
                             </tr>
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="py-4 px-6 font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th scope="row" className="py-4 px-6 font-medium text-gray-500 whitespace-nowrap dark:text-white">
                                     2
                                 </th>
-                                <td class="py-4 px-6">
+                                <td className="py-4 px-6">
                                     Minuman
                                 </td>
-                                <td class="py-4 px-6">
+                                <td className="py-4 px-6">
                                     2 months ago
                                 </td>
-                                <td class="py-4 px-6">
+                                <td className="py-4 px-6">
                                     2 months ago
                                 </td>
-                                <td class="py-4 px-6 flex justify-end gap-4">
-                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                    <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
+                                <td className="py-4 px-6 flex justify-end gap-4">
+                                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    <a href="#" className="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
                                 </td>
                             </tr>
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="py-4 px-6 font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th scope="row" className="py-4 px-6 font-medium text-gray-500 whitespace-nowrap dark:text-white">
                                     3
                                 </th>
-                                <td class="py-4 px-6">
+                                <td className="py-4 px-6">
                                     Cookies
                                 </td>
-                                <td class="py-4 px-6">
+                                <td className="py-4 px-6">
                                     2 months ago
                                 </td>
-                                <td class="py-4 px-6">
+                                <td className="py-4 px-6">
                                     2 months ago
                                 </td>
-                                <td class="py-4 px-6 flex justify-end gap-4">
-                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                    <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
+                                <td className="py-4 px-6 flex justify-end gap-4">
+                                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    <a href="#" className="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
                                 </td>
                             </tr>
                         </tbody>
