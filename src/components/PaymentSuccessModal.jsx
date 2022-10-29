174855aa4ae02768abf1b30a2ca61f5314bce592
Elvan Fiernes Ernando <elvanfiernesernando@gmail.com>
2022-10-29 12:58:18 +0700

    Fix comment

--- src/components/PaymentSuccessModal.jsx
+++ src/components/PaymentSuccessModal.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { HiOutlineX, HiOutlineCheck } from "react-icons/hi";
+import { HiOutlineX } from "react-icons/hi";
 import { PaymentContext } from '../stores/PaymentProvider';
 import Lottie from "lottie-react";
 import successAnimation from "../assets/lottie/successAnimation.json";
@@ -29,13 +29,12 @@ export default function PaymentSuccessModal() {
     return (
         <div className='w-screen h-screen fixed top-0 left-0 right-0 z-[5] flex justify-center items-center'>
 
-            {/* payment confirmation modal */}
+            {/* payment success modal */}
             <div className='w-[400px] bg-white rounded-lg shadow-lg p-8 flex flex-col gap-10 relative'>
 
                 {/* modal header */}
                 <div className='w-full flex flex-col items-center'>
                     <div className='bg-green-200 flex justify-center items-center h-20 w-20 rounded-full m-auto mb-3'>
-                        {/* <HiOutlineCheck className='text-2xl text-green-500' /> */}
                         <Lottie animationData={successAnimation} loop={true} />
                     </div>
                     <div>
